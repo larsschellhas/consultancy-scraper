a030395ee3315051d3e940f5cc452dc9785f73c8
Lars Schellhas <lars.schellhas@outlook.com>
2021-07-05 10:24:37 +0200

    Remove unnecessary use of urllib

--- src/consultancy_scraper/spiders/consulting_spider.py
+++ src/consultancy_scraper/spiders/consulting_spider.py
@@ -1,6 +1,5 @@
 import scrapy
 import re
-import urllib.parse
 from bs4 import BeautifulSoup
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
@@ -21,17 +20,17 @@ class ConsultingSpiderSpider(CrawlSpider):
     def parse_item(self, response):
         item = Consultancy()
         item['consulting_de_url'] = response.url
-        item['company_name'] = urllib.parse.unquote(re.sub('^[\s\xA0]+|[\s\xA0]+$', '', response.xpath('/html/body/div/div[2]/div[5]/div[1]/div[1]/div/article/div[3]/div/div[1]/div/div/div[1]/p/text()[1]').get()))
-        item['company_street'] = urllib.parse.unquote(re.sub('^[\s\xA0]+|[\s\xA0]+$', '', response.xpath('/html/body/div/div[2]/div[5]/div[1]/div[1]/div/article/div[3]/div/div[1]/div/div/div[1]/p/text()[3]').get()))
+        item['company_name'] = re.sub('^[\s\xA0]+|[\s\xA0]+$', '', response.xpath('/html/body/div/div[2]/div[5]/div[1]/div[1]/div/article/div[3]/div/div[1]/div/div/div[1]/p/text()[1]').get())
+        item['company_street'] = re.sub('^[\s\xA0]+|[\s\xA0]+$', '', response.xpath('/html/body/div/div[2]/div[5]/div[1]/div[1]/div/article/div[3]/div/div[1]/div/div/div[1]/p/text()[3]').get())
         plz_city = response.xpath('/html/body/div/div[2]/div[5]/div[1]/div[1]/div/article/div[3]/div/div[1]/div/div/div[1]/p/text()[4]').get()
-        plz_city = re.sub('^[\s\xA0]+|[\s\xA0]+$', '', urllib.parse.unquote(plz_city)).split(' ')
+        plz_city = re.sub('^[\s\xA0]+|[\s\xA0]+$', '', plz_city).split(' ')
         item['company_PLZ'] = plz_city.pop(0)
         item['company_city'] = ' '.join(plz_city)
-        item['company_country'] = urllib.parse.unquote(re.sub('^[\s\xA0]+|[\s\xA0]+$', '', response.xpath('/html/body/div/div[2]/div[5]/div[1]/div[1]/div/article/div[3]/div/div[1]/div/div/div[1]/p/text()[5]').get()))
-        item['company_phone'] = urllib.parse.unquote(re.sub('[\(\) a-zA-Z\.\:-]','', re.sub('^[\s\xA0]+|[\s\xA0]+$', '', response.xpath('/html/body/div/div[2]/div[5]/div[1]/div[1]/div/article/div[3]/div/div[1]/div/div/div[2]/p/text()[1]').get()).replace('(0)','')))
-        soup = BeautifulSoup(urllib.parse.unquote(re.sub('^[\s\xA0]+|[\s\xA0]+$', '', response.xpath('/html/body/div/div[2]/div[5]/div[1]/div[1]/div/article/div[3]/div/div[1]/div/div/div[2]/p/a[1]').get())))
+        item['company_country'] = re.sub('^[\s\xA0]+|[\s\xA0]+$', '', response.xpath('/html/body/div/div[2]/div[5]/div[1]/div[1]/div/article/div[3]/div/div[1]/div/div/div[1]/p/text()[5]').get())
+        item['company_phone'] = re.sub('[\(\) a-zA-Z\.\:-]','', re.sub('^[\s\xA0]+|[\s\xA0]+$', '', response.xpath('/html/body/div/div[2]/div[5]/div[1]/div[1]/div/article/div[3]/div/div[1]/div/div/div[2]/p/text()[1]').get()).replace('(0)',''))
+        soup = BeautifulSoup(response.xpath('/html/body/div/div[2]/div[5]/div[1]/div[1]/div/article/div[3]/div/div[1]/div/div/div[2]/p/a[1]').get())
         item['company_email'] = soup.a.text
-        soup = BeautifulSoup(urllib.parse.unquote(re.sub('^[\s\xA0]+|[\s\xA0]+$', '', response.xpath('/html/body/div/div[2]/div[5]/div[1]/div[1]/div/article/div[3]/div/div[1]/div/div/div[2]/p/a[2]').get())))
+        soup = BeautifulSoup(response.xpath('/html/body/div/div[2]/div[5]/div[1]/div[1]/div/article/div[3]/div/div[1]/div/div/div[2]/p/a[2]').get())
         item['company_website'] = soup.a.get('href')
-        item['company_type'] = urllib.parse.unquote(re.sub('^[\s\xA0]+|[\s\xA0]+$', '', response.xpath('/html/body/div/div[2]/div[5]/div[1]/div[1]/div/article/div[1]/div/p/text()').get()).replace('\r\n\t\t\t\t\t-', '')).split(', ')
+        item['company_type'] = re.sub('^[\s\xA0]+|[\s\xA0]+$', '', response.xpath('/html/body/div/div[2]/div[5]/div[1]/div[1]/div/article/div[1]/div/p/text()').get()).replace('\r\n\t\t\t\t\t-', '').split(', ')
         return item
